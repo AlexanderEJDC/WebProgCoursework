7e43a63d59ca46ca477a86abca1b498f3b9135fa
AlexanderEJDC <alexcock27@gmail.com>
2023-03-13 18:43:45 +0000

    adds a new default table row to the main page(?)

--- clientside/index.js
+++ clientside/index.js
@@ -1,2 +1,37 @@
 // Write our functions and other scripts here
 
+function removeContentFromElems(elem)
+{
+    elem.textContent = "";
+} //Supply an element, remove its text content. 
+
+function addDefaultRow(Msgs, location) 
+{//SHOULD create a tableRow, then for every default Message load it into a tableDataCell, appended to TR, append to table. 
+    const tr = document.createElement('tr');
+    for (const Msg of Msgs)
+    {
+        const td = document.createElement('td');
+        td.textContent = Msg;
+        tr.append(td)
+    }
+    location.append(tr); 
+}//ACTUALLY does
+
+async function LoadMessages()
+{//SHOULD fetch messages, then if it got a valid response, set them from response json. Otherwise, set it to "Bugger" if couldnt retrieve. 
+    const response = await fetch('Msgs');
+    let Msgs;
+    if (response.ok) {Msgs = await response.json();}
+    else {Msgs = ['Bugger.'];}
+    // Then find the table, remove prior content(?), then add the new default row. 
+    const table = document.querySelector('#mainTable');
+    removeContentFrom(table);
+    addDefaultRow(Msgs, table);
+}//ACTUALLY does
+
+function loadPage()
+{
+    LoadMessages();
+}
+
+loadPage();
--- server.js
+++ server.js
@@ -2,4 +2,20 @@ import express from 'express'; //Imports express module to enable certain featur
 
 const application = express();
 application.use(express.static('client'));
-application.listen(8080);
+
+const Msgs = 
+[
+    "07/03/2002",
+    "Got born.",
+    "Gained knowledge of how to start breathing.",
+    "Compentcy of ISO-00000001 how to live used."
+];
+
+function getMessages(request, response)
+{//Turn Messages into a json to throw at index.js
+    response.json(Msgs);
+}
+
+application.get('/Msgs', getMessages);
+
+application.listen(8080); //Listen to port 8080, as default 80 for HTTP is restricted. 
