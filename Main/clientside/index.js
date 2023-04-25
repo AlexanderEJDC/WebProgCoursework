ec6d41c8c349b5f25191cc695c9c184c033e5e67
AlexanderEJDC <alexcock27@gmail.com>
2023-04-25 10:30:39 +0100

    First attempt

--- Main/clientside/index.js
+++ Main/clientside/index.js
@@ -1,5 +1,16 @@
 // Write our functions and other scripts here
 
+import { send } from "process";
+
+//add a message if enter pressed
+function checkKeys(e) 
+{
+    if (e.key === 'Enter') 
+    {
+      //sendMessage(); SEND MESSAGE FUNCTION HERE!!
+    }
+  }
+
 function removeContentFromElems(elem)
 {
     elem.textContent = "";
@@ -17,6 +28,32 @@ function addDefaultRow(Msgs, location)
     location.append(tr); 
 }
 
+async function sendMessage()
+{//Send this to the server
+    const msgPayload = { msg: el.message.value }
+    //console.log('Payload', payload);
+
+    const response = await fetch('messages',
+    {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(msgPayload)
+    }); 
+
+    if (response.ok)
+    {
+        el.message.value = '';
+        const updatedMsgs = await response.json(); 
+        cloneTableRow(); 
+    } 
+    else { console.log("Failed to send message"); }
+}
+
+function populateWithTemplates()
+{
+
+}
+
 async function LoadMessages()
 {//SHOULD fetch messages, then if it got a valid response, set them from response json. Otherwise, set it to "Bugger" if couldnt retrieve. 
     const response = await fetch('Msgs');
@@ -29,6 +66,21 @@ async function LoadMessages()
     addDefaultRow(Msgs, table);
 }//ACTUALLY does
 
+function prepareHandles()
+{//Setup elements used in the code here.
+    el.tableParent = document.querySelectorAll(".")
+    el.suppliedDate = document.querySelectorAll(".dateInput");
+    el.suppliedWork = document.querySelectorAll(".workInput");
+    el.suppliedXP = document.querySelectorAll(".xpInput");
+    el.suppliedComp = document.querySelectorAll(".compInput");
+    el.submissionBtn = document.querySelectorAll(".submissionBtn");
+}
+
+function addEventListeners()
+{//Setup button listeners. TODO: Also keyboard strokes
+    submissionBtn.addEventListeners("click", sendMessage); 
+}
+
 
 //FUNCTIONS FOR CLONING TEMPLATES GO HERE
 function cloneTableHeader()
@@ -37,17 +89,20 @@ function cloneTableHeader()
     const clonedTHeader = tableHeader.cloneNode(true);
     document.body.append(clonedTHeader);
 }
-function cloneTableRow()
+function cloneTableRow(data)
 {
     const tableRow = document.querySelector("#tableRowTemplate");
     const cloneTableRow = tableRow.cloneNode(true);
+    
     //TODO: Figure out a way to add text content to the individual classes
     //Preferably make it an import straight from the message. 
-    document.body.append(cloneTableRow);
+    table.append(cloneTableRow);
 }
 
 function loadPage()
 {
+    prepareHandles(); 
+    addEventListeners();
     LoadMessages();
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Coursework for Web Programming UoP
 
 Commanders required to run:
+
+    PLAN: Populate page with template
+
+    THEN grab input from box to send to server
+
+    THEN append new table row to the table 
